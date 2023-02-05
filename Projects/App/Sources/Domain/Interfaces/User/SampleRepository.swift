2c935074b354251376beb345c9bc78ce56b755ab
Chanhee Jeong <chan420420@gmail.com>
2023-02-06 07:08:53 +0900

    🔧 Tuist 추가
    기존프로젝트에 Tuist 적용

---
+++ Projects/App/Sources/Domain/Interfaces/User/SampleRepository.swift
@@ -0,0 +1,15 @@
+////
+////  SampleRepository.swift
+////  www
+////
+////  Created by Chanhee Jeong on 2023/01/31.
+////
+//
+//import RxSwift
+//
+//protocol SampleRepository {
+//    
+//    // Methods
+//    func fetchSampleList() -> Single<Int>
+//    
+//}
---
+++ Projects/App/Sources/Presentation/Main/SampleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  www
+//
+//  Created by Chanhee Jeong on 2023/01/31.
+//
+
+import UIKit
+
+
+class SampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue
+    }
+
+}
+
