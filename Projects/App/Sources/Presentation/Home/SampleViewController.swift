7001013518e9fca88a71dd0bbbb4c565e68b8632
Chanhee Jeong <chan420420@gmail.com>
2023-02-07 03:29:06 +0900

    ✨ RootVC 를 NavigationVC 로 교체 및 샘플VC 수정

---
+++ Projects/App/Sources/Presentation/Home/SampleViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  www
+//
+//  Created by Chanhee Jeong on 2023/01/31.
+//
+
+import UIKit
+import SnapKit
+
+class SampleViewController: UIViewController {
+    
+    private lazy var testButton: LargeButton = {
+        $0.setTitle("테스트버튼", for: .normal)
+        $0.addTarget(self, action: #selector(testButtonDidTap), for: .touchUpInside)
+        return $0
+    }(LargeButton(state: true))
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+
+    private func setUI() {
+        self.view.backgroundColor = .white
+        view.addSubviews(testButton)
+        testButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+    }
+    
+    @objc func testButtonDidTap() {
+        self.navigationController?.pushViewController(RoomNameViewController(), animated: true)
+    }
+}
+
