9b95073f615bd72ba45af7d18e2e5546b30ce4f2
Chanhee Jeong <chan420420@gmail.com>
2023-02-08 03:31:41 +0900

    🍱 UIColor Extension 추가

---
+++ Projects/App/Sources/Utils/Extension/UIColor+.swift
@@ -0,0 +1,45 @@
+//
+//  UIColor+.swift
+//  App
+//
+//  Created by Chanhee Jeong on 2023/02/08.
+//  Copyright © 2023 com.promise8. All rights reserved.
+//
+
+
+import UIKit
+
+/// 🎨 WWW Color 사용가이드
+///
+/// ```
+/// self.view.backgroundColor = UIColor.wwwColor(.WWWGreen)
+/// ```
+
+enum WWWColor: String {
+    case WWWGreen
+    
+    case WWWBlack
+    case WWWWhite
+    case Gray800
+    case Gray750
+    case Gray700
+    case Gray650
+    case Gray600
+    case Gray550
+    case Gray500
+    case Gray450
+    case Gray400
+    case Gray350
+    case Gray300
+    case Gray250
+    case Gray200
+    case Gray150
+    case Gray100
+    case Gray50
+}
+
+extension UIColor {
+    static func wwwColor(_ color: WWWColor) -> UIColor {
+        return UIColor(named: color.rawValue)!
+    }
+}
