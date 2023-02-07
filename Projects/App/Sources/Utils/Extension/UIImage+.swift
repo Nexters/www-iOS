7f34b6c993a2ad4353528c0d841fa2b3afe1254c
Chanhee Jeong <chan420420@gmail.com>
2023-02-08 03:47:03 +0900

    🍱 UIImage+ 및 아이콘추가

---
+++ Projects/App/Sources/Utils/Extension/UIImage+.swift
@@ -0,0 +1,19 @@
+//
+//  UIImage+.swift
+//  App
+//
+//  Created by Chanhee Jeong on 2023/02/08.
+//  Copyright © 2023 com.promise8. All rights reserved.
+//
+
+import UIKit
+
+public extension UIImage {
+    convenience init?(_ asset: WWWAssset) {
+        self.init(named: asset.rawValue, in: Bundle.module, with: nil)
+    }
+
+    convenience init?(assetName: String) {
+        self.init(named: assetName, in: Bundle.module, with: nil)
+    }
+}
