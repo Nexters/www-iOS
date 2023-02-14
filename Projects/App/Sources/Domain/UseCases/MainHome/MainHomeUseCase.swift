ee69cdc10340b07d218ed58738efa9cac544957e
kokojong <duwhd0349@kookmin.ac.kr>
2023-02-14 23:09:38 +0900

    :sparkles: MainHomeUseCase

---
+++ Projects/App/Sources/Domain/UseCases/MainHome/MainHomeUseCase.swift
@@ -0,0 +1,32 @@
+//
+//  MainHomeUseCase.swift
+//  App
+//
+//  Created by kokojong on 2023/02/13.
+//  Copyright © 2023 com.promise8. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+protocol MainHomeUseCaseProtocol {
+    var proceedingMeetings: BehaviorSubject<[String]> { get }
+    var endedMeetings: BehaviorSubject<[String]> { get }
+}
+
+final class MainHomeUseCase: MainHomeUseCaseProtocol {
+    
+    private let meetingRepository: MeetingRepository
+    
+    init(meetingRepository: MeetingRepository) {
+        self.meetingRepository = meetingRepository
+    }
+    
+    var proceedingMeetings = BehaviorSubject<[String]>(value: [])
+    
+    var endedMeetings = BehaviorSubject<[String]>(value: [])
+    
+    func fetchMainHomeMeeting() -> Single<MainHomeMeeting> {
+        meetingRepository.fetchMainHomeMeeting()
+    }
+}
