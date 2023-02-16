b9ecda6ef906ceeb1fab650e68fe9ed30f2f03e2
kokojong <duwhd0349@kookmin.ac.kr>
2023-02-16 15:59:59 +0900

    :sparkles: UIView+ setRoundCorner 추가

--- Projects/App/Sources/Utils/Extension/UIView+.swift
+++ Projects/App/Sources/Utils/Extension/UIView+.swift
@@ -8,9 +8,20 @@
 import UIKit
 
 extension UIView {
-  func addSubviews(_ views: UIView...) {
-    for view in views {
-      addSubview(view)
+    func addSubviews(_ views: UIView...) {
+        for view in views {
+            addSubview(view)
+        }
     }
-  }
+    
+    func setRoundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = radius
+        var masked = CACornerMask()
+        if corners.contains(.topLeft) { masked.insert(.layerMinXMinYCorner) }
+        if corners.contains(.topRight) { masked.insert(.layerMaxXMinYCorner) }
+        if corners.contains(.bottomLeft) { masked.insert(.layerMinXMaxYCorner) }
+        if corners.contains(.bottomRight) { masked.insert(.layerMaxXMaxYCorner) }
+        self.layer.maskedCorners = masked
+    }
 }
