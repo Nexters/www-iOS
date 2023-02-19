14f85af39068e7357e25be547a2784a5ce4fdb06
Chanhee Jeong <chan420420@gmail.com>
2023-02-19 20:49:50 +0900

    📦 DeviceKit SPM 추가

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -22,7 +22,9 @@ let dependencies = Dependencies(
         .remote(
             url: "https://github.com/Moya/Moya.git",
             requirement: .upToNextMajor(from: "15.0.0")
-        )
+        ),
+        .remote(url: "https://github.com/devicekit/DeviceKit.git",
+                requirement: .upToNextMajor(from: "4.0.0"))
     ],
     platforms: [.iOS]
 )
