376079ad6ea5b910754f26cbd05320f45ce91db3
Chanhee Jeong <chan420420@gmail.com>
2023-02-23 04:23:07 +0900

    💄 gradient extension 추가
    feat. chatgpt

--- Projects/App/Sources/Utils/Extension/UIView+.swift
+++ Projects/App/Sources/Utils/Extension/UIView+.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 extension UIView {
+    
     func addSubviews(_ views: UIView...) {
         for view in views {
             addSubview(view)
         }
     }
+    
+    
+    /// .addGradientLayer
+    /// - This will add a gradient layer with red to blue gradient, starting at the top-left corner of myView and ending at the bottom-right corner.
+    /// - REF : ChatGPT🤖
+    ///
+    /// ```
+    /// myView.addGradientLayer(colors: [UIColor.red.cgColor, UIColor.blue.cgColor], locations: nil, startPoint: CGPoint(x: 0, y: 0), endPoint: CGPoint(x: 1, y: 1))
+    /// ```
+    func addGradientLayer(colors: [CGColor], locations: [NSNumber]?, startPoint: CGPoint, endPoint: CGPoint) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = colors
+        gradientLayer.locations = locations
+        gradientLayer.startPoint = startPoint
+        gradientLayer.endPoint = endPoint
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
     
     func setRoundCorners(_ corners: UIRectCorner, radius: CGFloat) {
         self.clipsToBounds = true
