04cf6d777bacd7425b9ccabebb6abd50da9d0352
Chanhee Jeong <chan420420@gmail.com>
2023-03-01 05:37:40 +0900

    ✨ TextInputError 타입정의

---
+++ Projects/App/Sources/Utils/Constants/TextInputError.swift
@@ -0,0 +1,15 @@
+//
+//  TextInputError.swift
+//  App
+//
+//  Created by Chanhee Jeong on 2023/03/01.
+//  Copyright © 2023 com.promise8. All rights reserved.
+//
+
+import Foundation
+
+enum TextInputError {
+    case over
+    case under
+    case normal
+}
