ba58255d1a80e5e89373fd01275a3884a2b5fcd0
kokojong <duwhd0349@kookmin.ac.kr>
2023-03-02 07:00:26 +0900

    :poop: rootVC 변경

--- Projects/App/Sources/Application/SceneDelegate.swift
+++ Projects/App/Sources/Application/SceneDelegate.swift
@@ -15,19 +15,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
         
-//        let launchVC = LaunchScreenViewController(viewModel: LaunchScreenViewModel(joinUserUseCase: JoinUserUseCase(userRepository: UserDAO.init(network: UserAPIManager.provider))))
-//        window?.rootViewController = UINavigationController(rootViewController: launchVC)
-//
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-//            let mainHomeVC = MainHomeViewController(viewModel: MainHomeViewModel(mainHomeUseCase: .init(meetingRepository: MainHomeDAO.init(network: MeetingAPIManager.provider))))
-//            self.window?.rootViewController = UINavigationController(rootViewController: mainHomeVC)
-//        }
+        let launchVC = LaunchScreenViewController(viewModel: LaunchScreenViewModel(joinUserUseCase: JoinUserUseCase(userRepository: UserDAO.init(network: UserAPIManager.provider))))
+        window?.rootViewController = UINavigationController(rootViewController: launchVC)
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
             
             if UserDefaultKeyCase().getOnBoarding() {
                 let mainHomeVC = MainHomeViewController(viewModel: MainHomeViewModel(mainHomeUseCase: .init(meetingRepository: MainHomeDAO.init(network: MeetingAPIManager.provider))))
-                self.window?.rootViewController = UINavigationController(rootViewController: PlaceVoteViewController(viewmodel: PlaceVoteViewModel(usecase: PlaceVoteUseCase(), roomId: 193, status: .voting)))
+                self.window?.rootViewController = UINavigationController(rootViewController: mainHomeVC)
+                
+                // roomMainVC
+                let roomMainVC = RoomMainViewController(viewModel: RoomHomeViewModel(mainRoomUseCase: MainRoomUseCase(meetingRoomRepository: MainRoomDAO(network: MeetingAPIManager.provider))))
+                self.window?.rootViewController = UINavigationController(rootViewController: roomMainVC)
+                
             } else {
                 lazy var onBoarding = OnBoardingPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
                 self.window?.rootViewController = UINavigationController(rootViewController: onBoarding)
